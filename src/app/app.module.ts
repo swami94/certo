8312dcb528da5f19f4a447e21feb406f5e608d3b
swami94 <pothalaswami@gmail.com>
2025-12-08 12:54:47 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './commoncomponents/navbar/navbar.component';
+import { FooterComponent } from './commoncomponents/footer/footer.component';
+import { IphoneComponent } from './navigatingcomponents/iphone/iphone.component';
+import { AndroidComponent } from './navigatingcomponents/android/android.component';
+import { HelpComponent } from './navigatingcomponents/help/help.component';
+import { CompanyComponent } from './navigatingcomponents/company/company.component';
+import { SigninComponent } from './navigatingcomponents/signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     PagenotfoundComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    IphoneComponent,
+    AndroidComponent,
+    HelpComponent,
+    CompanyComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
